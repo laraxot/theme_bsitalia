d85f514e310237f816f9f8a77f52099e8c9835d9
marco76tv <marco.sottana@gmail.com>
2022-09-06 14:50:34 +0200

    up

---
+++ Resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+@extends('pub_theme::errors.minimal')
+
+@section('title', __('Unauthorized'))
+@section('code', '401')
+@section('message', __('Unauthorized'))
---
+++ Resources/views/errors/429.blade.php
@@ -0,0 +1,5 @@
+@extends('pub_theme::errors.minimal')
+
+@section('title', __('Too Many Requests'))
+@section('code', '429')
+@section('message', __('Too Many Requests'))
