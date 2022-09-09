48858a9d81b7079cc72ce5df94b69808b54591f4
Malebestia <malebestia@yahoo.it>
2022-09-09 17:44:50 +0200

    .

---
+++ Resources/views/layouts/partials/footer/logo.blade.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-12 footer-items-wrapper logo-wrapper">
+        <img class="ue-logo" src="../assets/images/logo-eu-inverted.svg" alt="logo Unione Europea">
+        <div class="it-brand-wrapper">
+            <a href="#">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-pa"></use>
+                </svg>
+                <div class="it-brand-text">
+                    <h2 class="no_toc">Nome del Comune</h2>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
