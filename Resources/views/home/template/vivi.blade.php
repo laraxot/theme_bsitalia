1173a148cc13a4062bd6570cd7bc94202368e5cd
marco xot <marco.sottana@gmail.com>
2022-10-05 17:38:19 +0200

    up

--- Resources/views/home/template/vivi.blade.php
+++ Resources/views/home/template/vivi.blade.php
@@ -23,8 +23,9 @@
         d’interesse per scoprire e vivere il territorio comunale." wrapper-class="pt-0 ps-0 pb-4 pb-lg-70"}}
           --}}
             <x-hero type="hero">
-                <slot name="title">Vivere il comune</slot>
-                <slot name="txt">Tutti gli eventi, le iniziative e i luoghi d’interesse per scoprire e vivere il territorio comunale</slot>
+                <x-slot:wrapper class="pt-0 ps-0 pb-4 pb-lg-70">AA</x-slot>
+                <x-slot name="title">Vivere il comune</x-slot>
+                <x-slot name="txt">Tutti gli eventi, le iniziative e i luoghi d’interesse per scoprire e vivere il territorio comunale</x-slot>
             </x-hero>
         </div>
       </div>
@@ -35,29 +36,37 @@
       {{>cmp-hero-img/cmp-hero-img-small description="Colosseo, Roma. Foto di Griffin Wooldridge." img-path="roma-colosseo.png"}}
       --}}
       <x-hero type="img-small">
-          <slot name="txt">Colosseo, Roma. Foto di Griffin Wooldridge.</slot>
-          <slot name="img_src">roma-colosseo.png</slot>
+          <x-slot name="txt">Colosseo, Roma. Foto di Griffin Wooldridge.</x-slot>
+          <x-slot name="img_src">roma-colosseo.png</x-slot>
       </x-hero>
     </section>
 
     <div class="bg-grey-dsk py-5">
       <div class="container">
         <h2 class="title-xxlarge mb-4">Eventi in evidenza</h2>
+        {{--
         {{>cmp-list-card-img/cmp-list-card-img cards=cards.card-events label="Tutti gli eventi" buttonNext=true}}
+        --}}
+        <x-card.rows type="img" :rows="$_theme->getCardEvents()" buttonNext=true></x-card.rows>
       </div>
     </div>
 
     <div class="container p-3 p-md-5">
       <h2 class="title-xxlarge mb-40 ">Luoghi in evidenza</h2>
-
+      {{--
       {{>cmp-list-card-img/cmp-list-card-img cards=cards.card-places label="Tutti i luoghi" buttonNext=true}}
+      --}}
+      <x-card.rows type="img" :rows="$_theme->getCardPlaces()" buttonNext=true></x-card.rows>
     </div>
 
     <div class="bg-primary">
       <div class="container">
         <div class="row d-flex justify-content-center bg-primary">
           <div class="col-12 col-lg-6">
+            {{--
             {{>cmp-rating/cmp-rating public-template=true}}
+            --}}
+            <x-rating public-template=true ></x-rating>
           </div>
         </div>
       </div>
@@ -67,7 +76,10 @@
       <div class="container">
         <div class="row d-flex justify-content-center p-contacts">
           <div class="col-12 col-lg-6">
+            {{--
             {{>cmp-contacts/cmp-contacts city-problems=true}}
+            --}}
+
           </div>
         </div>
       </div>
