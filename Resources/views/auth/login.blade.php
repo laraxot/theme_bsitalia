49ecbddef0dba425710813e06fe3570166701458
Malebestia <malebestia@yahoo.it>
2022-10-19 19:39:41 +0200

    up

--- Resources/views/auth/login.blade.php
+++ Resources/views/auth/login.blade.php
@@ -1,90 +1,68 @@
 @php
-//NOTA BENE, ESEMPIO PAGINA DI LOGIN QUI
-//https://docs.italia.it/accounts/login/
+    //NOTA BENE, ESEMPIO PAGINA DI LOGIN QUI
+    //https://docs.italia.it/accounts/login/
 @endphp
 
+@extends('pub_theme::layouts.app')
+@section('content')
+    <section class="container mt-5" style="max-width: 40em">
+        <h1 class="pb-5">Log in</h1>
+        <form class="login" method="POST" action="{{ route('login', ['referrer' => $referrer]) }}">
+            @csrf
+            {{-- <input type='hidden' name='csrfmiddlewaretoken'
+                value='saxz92N0NAOO95HAGWI3Q9PIgo1xBhpw32xXbwHIlxTX2Q2O5qEnstLOKTxUMqXz' /> --}}
 
-<x-guest-layout>
-    <x-authentication.card>
-        <x-slot name="logo">
-            <x-authentication.card-logo />
-        </x-slot>
+            <x-input.group label="Email Address" type="email" name="email" col_size="12" id="id_login"></x-input.group>
 
-        <div class="card-body">
 
-            {{-- <x-validation.errors class="mb-3 rounded-0" />
+            {{-- <div class="form-group">
+                <input type="email" name="email" class="form-control " id="id_login">
+                <label for="id_login">Email</label>
+            </div> --}}
 
-            @if (session('status'))
-                <div class="alert alert-success mb-3 rounded-0" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif --}}
 
-            <x-input.errors></x-input.errors>
-            <form method="POST" action="{{ route('login', ['referrer' => $referrer]) }}">
-                @csrf
-                {{-- <div class="form-group">
-                    <x-input.label value="{{ __('Email') }}" />
-
-                    <x-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email"
-                        :value="old('email')" required />
-                    <x-input.error for="email"></x-input.error>
-                </div>
-
-                <div class="form-group">
-                    <x-input.label value="{{ __('Password') }}" />
-
-                    <x-input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password"
-                        name="password" required autocomplete="current-password" />
-                    <x-input.error for="password"></x-input.error>
-                </div> --}}
-
-                <x-input.group label="Email Address" type="email" name="email" colSize="12"></x-input.group>
-
-                <x-input.group label="Password" type="password" name="password" colSize="12"></x-input.group>
-
-                {{-- <div class="form-group">
-                    <div class="form-check">
-                        <x-forms.inputs.checkbox id="remember_me" name="remember" />
-
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div> --}}
-
-                <div class="form-group">
-                    <div class="form-check">
-                        <x-theme::checkbox id="remember_me" name="remember" />
-
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
-
-                <div class="mb-0">
-                    <div class="d-flex justify-content-end align-items-baseline">
-                        @if (Route::has('password.request'))
-                            <a class="text-muted mr-3" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <!-- Submit-->
-                        <div class="d-grid">
-                            <button class="btn btn-primary">Sign in</button>
-                        </div>
-                        {{-- <x-button title="Login">
-                            {{ __('Login') }}
-                        </x-button> --}}
-                    </div>
-                </div>
-                <p class="text-center">
-                    <small class="text-muted text-center">Don't have an account yet?
-                        <a href="{{ url('it/register') }}">Sign Up </a></small>
-                </p>
-            </form>
-        </div>
-    </x-authentication.card>
-    </x-layouts.guest>
+            {{-- <div class="form-group">
+                <input type="text" name="login" class="form-control " id="id_login">
+                <label for="id_login">Login</label>
+            </div> --}}
+            <div class="form-group">
+                <input type="password" name="password" class="form-control input-password" id="id_password">
+                <label for="id_password">Password</label>
+                <span class="password-icon bg-transparent" aria-hidden="true">
+                    <svg class="password-icon-visible icon icon-sm">
+                        <use
+                            xlink:href="https://docs.italia.it/media/static/vendor/bootstrap-italia/svg/sprite.svg#it-password-visible">
+                        </use>
+                    </svg>
+                    <svg class="password-icon-invisible icon icon-sm d-none">
+                        <use
+                            xlink:href="https://docs.italia.it/media/static/vendor/bootstrap-italia/svg/sprite.svg#it-password-invisible">
+                        </use>
+                    </svg>
+                </span>
+            </div>
+            <div class="form-check">
+                <input id="id_remember" name="remember" type="checkbox">
+                <label for="id_remember">Ricordami</label>
+            </div>
+            <div class="py-4 login_buttons">
+                <button class="btn btn-primary" type="submit">Log in</button>
+                {{-- <span class="px-3 my-3 my-md-0">oppure</span>
+                <a class="btn btn-icon btn-outline-primary github" href="/accounts/github/login/?process=login"
+                    title="GitHub">
+                    <svg class="icon icon-sm icon-primary">
+                        <use
+                            xlink:href="https://docs.italia.it/media/static/vendor/bootstrap-italia/svg/sprite.svg#it-github">
+                        </use>
+                    </svg>
+                    <span>
+                        Autenticati con GitHub
+                    </span>
+                </a>
+                <p></p> --}}
+            </div>
+            <p>Se non hai ancora creato un account, <a href="/accounts/signup/">registrati</a> adesso.</p>
+            <p>Se invece hai dimenticato la password, puoi <a href="/accounts/password/reset/">reimpostarla</a>.</p>
+        </form>
+    </section>
+@endsection
