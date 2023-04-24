b0b0400b58414fd20454a3fa18dbc7cc96afef5a
Malebestia <vair81@gmail.com>
2023-04-24 15:43:43 +0200

    up

--- Config/button.php
+++ Config/button.php
@@ -2,5 +2,13 @@
   'v1' => 
   array (
     'class' => 'btn btn-primary',
+    'button' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+    'submit' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
   ),
 );
